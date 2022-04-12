a9d3a03894a50914f1d6e55a6de35e16f60f9130
Prakash <prakashsingha1098@gmail.com>
2022-04-12 14:29:20 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter >
+         <Navbar />
+        <Routes>
+        
+
+        <Route exact path="/" element={<Home />}/>
+         
+        <Route  path="/create" element={<Create />}/>
+
+        <Route  path="/search" element={<Search />}/>
+        
+        <Route  path="/recipe/:id" element={<Recipe />}/>
+     
+   
+        </Routes>
+     
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+import { useFetch } from '../../hooks/4.1 useFetch'
+import RecipeList from '../../components/RecipeList'
+export default function Home(){
+    const {data,isPending,error} = useFetch('http://localhost:3000/recipes')
+    return(
+        <div className='home'>
+           {error && <p className='error'>{error}</p>}
+           {isPending && <p className='loading'>Loading..</p>}
+           {data && <RecipeList recipes={data}/>}
+        </div>
+    )
+}
